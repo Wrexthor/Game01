7386f54dc50747e55dafefd7323a93cff5862d4e
jack <wrexthor@protonmail.com>
2017-04-03 13:41:39 +0200

    initial

---
+++ Game01.py
@@ -0,0 +1,26 @@
+# player class
+# fame variable (int)
+# dread variable (int)
+# supplies variable (int)
+# inventory array (obj)
+# traits variable (int)
+# followers variable (int)
+# name varaible (string)
+
+
+# npc base class
+# hit points variable (int)
+# damage variable (int)
+# name variable (string)
+# amount variable (int)
+
+# npc sub classes
+# types available, slave, merchant, fighter, cheiftan
+
+# menu class
+# takes choice of player
+
+# event class
+# based on input parameters (like fame, dread etc)
+# outputs events
+
